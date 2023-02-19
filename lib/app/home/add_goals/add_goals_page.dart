6ae90012eb20e0052a5a02590df8e94b1a56746f
Natalia Krawczuk <nkravchuk60@gmail.com>
2023-02-19 15:17:30 +0100

    Implement adding goals

--- lib/app/home/add_goals/add_goals_page.dart
+++ lib/app/home/add_goals/add_goals_page.dart
@@ -1,13 +1,64 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AddPageContent extends StatelessWidget {
+class AddPageContent extends StatefulWidget {
   const AddPageContent({
     Key? key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(child: Text('dwa'));
-  }
+  State<AddPageContent> createState() => _AddPageContentState();
 }
 
+class _AddPageContentState extends State<AddPageContent> {
+  var goalTitle = '';
+  var goalContent = '';
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: TextField(
+            decoration: const InputDecoration(
+              hintText: 'Podaj nazwe Twojego zadania',
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                goalTitle = newValue;
+              });
+            },
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: TextField(
+            decoration: const InputDecoration(
+              hintText: 'Podaj tresc Twojego zadania',
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                goalContent = newValue;
+              });
+            },
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: ElevatedButton(
+            onPressed: () {
+              FirebaseFirestore.instance.collection('goals').add({
+                'title': goalTitle,
+                'goal': goalContent,
+                'rating': 3.0,
+              });
+            },
+            child: const Text('Dodaj'),
+          ),
+        ),
+      ],
+    ));
+  }
+}
